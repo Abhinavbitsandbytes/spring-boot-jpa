96a915b8cc2e7902fa46ae05424dde8266b0e0d3
Abhinav <abhinavkr197@gmail.com>
2025-12-30 19:51:23 +0530

    3.2- hibernate-orm-jpa-entities-and-tables-in-spring-data-jpa

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO test.product_table (price, quantity, sku, title_x)
+VALUES
+    (12.4, 4, 'parle734', 'Parle Biscuit'),
+    (14.4, 1, 'pepsi123', 'Pepsi'),
+    (34.4, 1, 'coke124', 'Coca Cola'),
+    (5.4, 10, 'lays345', 'Lays Chips'),
+    (16.4, 3, 'pepsi156', 'Mazza');
